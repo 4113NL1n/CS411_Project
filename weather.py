fff3f6b8934f27852167dfb12a0f79e112425695
Allen Lin <allenlin@bu.edu>
2024-12-06 22:10:54 -0500

    get weather

--- weather.py
+++ weather.py
@@ -2,9 +2,24 @@ import requests
 from dotenv import load_dotenv
 import os
 import json 
+from dataclasses import dataclass
+
+load_dotenv()
 api_key = os.getenv('API_KEY')
- 
+@dataclass
+class info:
+    main : str 
+    location : str
+    high : float 
+    curr : float
+    low : float 
+    
 def get_weather(city_name,API_key):
-    response = json.loads (requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}").json())
-    print(response)
-get_weather("nEW yORK", api_key)
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}").json()
+    data = info(
+        main = response.get("weather")[0].get("main"),
+        location = response.get("sys").get("country"),
+        high = response.get("main").get("temp_max"),
+        curr = response.get("main").get("temp"),
+        low = response.get("main").get("temp")
+    )
