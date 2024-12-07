0f983b2ba048add86ba6ccbb314286391a50d8e3
Allen Lin <allenlin@bu.edu>
2024-12-07 17:10:37 -0500

    added forecast

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from utils.sql_utils import execute_script,get_db_connection
 from models.user_models import create_user
+from weather import search_weather
 import sqlite3
 def create_db():
     print("Starting the script...")
@@ -8,7 +9,6 @@ def create_db():
         print("Database table created successfully.")
     except Exception as e:
         print(f"Error creating table: {e}")
-
 
 def get_user():
     try:
@@ -22,10 +22,15 @@ def get_user():
     except Exception as e:
         raise e
 
-
 def create_acc(username, passw):
     if create_user(username,passw):
         print("Account success")
     else: 
         print("Account username taken")
 
+def get_weather(city):
+    data = search_weather(city)
+    print(data)
+    
+if __name__ == "__main__":
+    get_weather("Boston")
--- weather.py
+++ weather.py
@@ -15,12 +15,35 @@ class info:
     low : float 
 
 # Call api and return weather condition, location, temp, high temp, low temp
+# first api call
 def get_weather(city_name,API_key):
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}").json()
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=imperial").json()
+    print(response)
     data = info(
         main = response.get("weather")[0].get("main"),
-        location = response.get("sys").get("country"),
+        location = response.get("name"),
         high = response.get("main").get("temp_max"),
         curr = response.get("main").get("temp"),
         low = response.get("main").get("temp")
     )
+    return data
+#second api call for the next 4  days
+def get_weather_forecast(city_name,API_key):
+    forecast = []
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_key}&units=imperial").json()
+    for i in range(1,6):
+        data = info(
+            main = response.get("list")[i*9].get("weather")[0].get("main"),
+            location = response.get("city").get("name"),
+            high = response.get("list")[i*9].get("main").get("temp_max"),
+            curr = response.get("list")[i*9].get("main").get("temp"),
+            low = response.get("list")[i*9].get("main").get("temp_min")
+            )
+        forecast.append(data)
+    return forecast
+
+def search_weather(city):
+    return get_weather(city, api_key)
+
+if __name__ == "__main__":
+    get_weather_forecast("boston",api_key)
