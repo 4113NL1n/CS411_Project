7ce1c2a30162bd1edce2150023f8445d11bc8312
Allen Lin <allenlin@bu.edu>
2024-12-08 22:04:11 -0500

    added unittest for user and weather models

--- model/weather_models.py
+++ model/weather_models.py
@@ -3,36 +3,33 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, make_response, Response, request
 load_dotenv()
 app = Flask(__name__)
+fave = []
 
-def __init__(self):
-    self.weathers = []
+@app.route('/weather/favorite', methods=['GET'])
+def get_weather_fave():
+    return jsonify(fave)
 
-    @app.route('/weather', methods=['GET'])
-    def get_weather_fave():
-        for i in list:
-            self.weathers.append(search_weather(i))
-        return jsonify(self.weathers)
-    
-    @app.route('/weather/<city>', methods=['GET'])
-    def get_weather(city):
-        data = search_weather(city)
-        return jsonify(data)
+@app.route('/weather/<city>', methods=['GET'])
+def get_weather(city):
+    data = search_weather(city)
+    return jsonify(data)
 
-    @app.route('/weather<city>', methods=['GET'])
-    def get_forecast(city):
-        data = search_forecastt(city)  
-        return jsonify(data)
+@app.route('/weather/forecast/<city>', methods=['GET'])
+def get_forecast(city):
+    data = search_forecastt(city)  
+    return jsonify(data)
 
-    @app.route('/weather/<city>/<state_code>/<country_code>', methods=['GET']) 
-    def get_air(city,state_code,country_code):
-        data = search_air_quality(city,state_code,country_code)  
-        return jsonify(data)
+@app.route('/weather/air/<city>/<state_code>/<country_code>', methods=['GET']) 
+def get_air(city,state_code,country_code):
+    data = search_air_quality(city,state_code,country_code)  
+    return jsonify(data)
 
-    @app.route('/weather/<city>', methods=['GET'])
-    def get_alerts(state_code):
-        return jsonify(search_alert(state_code))
+@app.route('/weather/alerts/<state_code>', methods=['GET'])
+def get_alerts(state_code):
+    return jsonify(search_alert(state_code))
 
-    @app.route('/weather/<city>', methods=['PUT'])
-    def save_favorite(name):
-        list.append(name)
-    
+@app.route('/weather/favorite/save/<city>', methods=['PUT'])
+def save_favorite(city):
+    fave.append(city)
+
+
---
+++ test/test_weather_model.py
@@ -0,0 +1,40 @@
+import pytest
+from flask import Flask, jsonify
+from model.weather_models import app
+from unittest.mock import patch
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+
+def test_get_weather(client):
+    response = client.get('/weather/Boston')
+    assert response.status_code == 200
+    
+def test_get_forecast(client):
+    response =client.get("/weather/forecast/Boston")
+    assert response.status_code == 200
+    
+def test_get_air(client):
+    response =client.get("/weather/air/Boston/MA/US")
+    assert response.status_code == 200
+
+def test_get_alerts(client):
+    response =client.get("/weather/alerts/NY")
+    assert response.status_code == 200
+
+def test_get_favorite(client):
+    fave = []
+    response = client.get("/weather/favorite")
+    assert response.status_code == 200
+    assert response.get_json() == fave
+    client.put("/weather/favorite/save/Boston")
+    fave = ["Boston"]
+    
+    # Step 4: Ensure that the favorite list now contains "Boston"
+    response = client.get("/weather/favorite")
+    assert response.status_code == 200
+    assert response.get_json() == fave 
